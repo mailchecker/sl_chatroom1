dccc914f5346fcccfcd5dc55eb1785cc0c8f06e1
mailchecker <zinux@naver.com>
2023-10-02 18:11:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-import streamlit as st, sqlite3, datetime#, os ; os.remove('chats.db')
-
-con=sqlite3.connect('chats.db')
-cur=con.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS db(messages TEXT)')
-
+import streamlit as st, datetime
+# import os ; os.remove('chat.txt') 
+# Uncomment the line above to delete the file containing
+# all messages, and start over with a fresh chat room.
 
 col1, col2=st.columns([2,3])
 with col2:
-  msg=cur.execute('SELECT messages FROM db').fetchone()
-  if msg==None:
-    cur.execute('INSERT INTO db(messages) VALUES ("")')
-    con.commit()
-  st.text_area('msg', msg[0], height=350, label_visibility='collapsed')
+  with open('chat.txt', 'a+') as file: pass
+  with open('chat.txt', 'r+') as file:
+    msg=file.read()
+    st.text_area('msg', msg, height=350, label_visibility='collapsed')
+
 with col1:
   with st.form('New Message', clear_on_submit=True):
     name=st.text_input('Name')
     message=st.text_area('Message') 
     timestamp=datetime.datetime.now()
     if st.form_submit_button('Add Message'):
-      newmsg=f'---  {name}   {timestamp}\n\n{message}\n\n{msg[0]}'
-      cur.execute(
-        'UPDATE db SET messages=? WHERE rowid=?;', 
-        (newmsg, 1)
-      )
-      con.commit()
+      newmsg=f'---  {name}   {timestamp}\n\n{message}\n\n{msg}'
+      with open('chat.txt', 'w') as file:
+        file.write(newmsg)
       st.experimental_rerun()
